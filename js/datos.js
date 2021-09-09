4dd8ecf365f5d68abf990018a9ed3075fba2f816
alejandrajlopez <82856819+alejandrajlopez@users.noreply.github.com>
2021-09-08 22:58:23 -0300

    Add files via upload

---
+++ js/datos.js
@@ -0,0 +1,35 @@
+/*Objeto JSON */
+
+const productos = [        
+    {
+        id: 1, 
+        nombre: "Tarot", 
+        precio: 1000, 
+        img:"img/tarot.jpg",
+    },
+        {
+        id: 2, 
+        nombre: "Gemas", 
+        precio: 900, 
+        img:"img/gemas.jpg", 
+    },
+        {
+        id: 3,
+        nombre: "Runas",
+        precio: 1500, 
+        img:"img/runas.jpg", 
+    },
+      {
+        id: 4,
+         nombre: "Números", 
+         precio: 1200,
+         img:"img/numerologia.jpg", 
+     },
+     {
+        id: 5,
+         nombre: "Flores de Bach", 
+         precio: 1200,
+         img:"img/flores.jpg", 
+     },
+      
+     ]
